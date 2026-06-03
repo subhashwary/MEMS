84c5f7ebfa5e8f2d1bde1d1d068e48f9713c6bbe
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-03 17:12:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-.instrument-row input {
-    width: 90px;
-    height: 30px;
-
-    text-align: center;
-
-    border: 1px solid #ccc;
-    border-radius: 6px;
+.ess-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: start;
+    gap: 20px;
 }
