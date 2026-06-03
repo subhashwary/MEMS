d6aae79b3c62ec516325d725172a26ac3bd7c50b
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-03 17:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-.instrument-box {
-    width: 350px;
-    height: auto;
-    min-height: 260px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.ps-title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    text-align: center;
 }
+
+.ps-head {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 15px;
+    text-align: center;
+}
+
