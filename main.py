39bb2a3e182cc7d916c31d92c453e467e63e8c03
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-03 16:57:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
-<div class="ess-box instrument-box">
+function stopPSU() {
 
-    <div class="ps-title">DC Power Supply</div>
+    fetch('/psu/stop', {
+        method: 'POST'
+    })
+    .then(res => res.json())
+    .then(data => {
 
-    <div class="ps-head" style="margin-bottom:20px;">
-        CH1
-    </div>
+        console.log(data);
 
-    <div class="instrument-row">
-        <span class="instrument-label">Set Voltage</span>
-
-        <input
-            id="psVoltage"
-            type="number"
-            step="0.01"
-            value="2.00">
-
-        <span>Volt</span>
-    </div>
-
-    <div class="instrument-row">
-        <span class="instrument-label">Set Current</span>
-
-        <input
-            id="psCurrent"
-            type="number"
-            step="0.01"
-            value="0.20">
-
-        <span>Amp</span>
-    </div>
-
-    <div class="ps-row-bottom">
-        <button
-            class="control-btn start-btn"
-            onclick="startPSU()">
-            ON
-        </button>
-
-        <button
-            class="control-btn stop-btn"
-            onclick="stopPSU()">
-            OFF
-        </button>
-    </div>
-
-</div>
+        if(data.error){
+            alert(data.error);
+        }
+    });
+}
