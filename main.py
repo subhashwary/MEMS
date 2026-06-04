35cb2caf4968e4c329d3c74dc253c68f5105a10a
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 14:25:34 +0530

    Implement multimeter connection and voltage measurement
    Added functionality to connect to a multimeter and read voltage continuously.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-Connecting to COM3...
-DMM ID:
-SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
-GWInstek,GDM8261A,GEO882227,1.02
-PS D:\Wary\MEMS> & d:/Wary/MEMS/venv/Scripts/python.exe d:/Wary/MEMS/test_dmm.py
-DMM ID:
-SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
-GWInstek,GDM8261A,GEO882227,1.02
-PS D:\Wary\MEMS> (Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& d:\Wary\MEMS\venv\Scripts\Activate.ps1)
+from instrument import Multimeter
+import time
+
+dmm = Multimeter("COM3")
+
+print("Connected:")
+print(dmm.idn())
+
+print("\nReading voltage...\n")
+
+while True:
+
+    voltage = dmm.measure_voltage()
+
+    print("Voltage =", voltage)
+
+    time.sleep(1)
