306a5dece9345d2580f004de8899b8da21be16fe
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 16:15:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,292 @@
-class PowerSupply(SCPIInstrument):
+(venv) PS D:\Wary\MEMS> python app.py
+ * Serving Flask app 'app'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /static/images/IISc.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /static/images/IISc.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /static/images/cense.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /static/images/cense.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /ports HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:24] "GET /ports HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:26] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:27] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:27] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:28] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:29] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:30] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:30] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:31] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:32] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:32] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:33] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:33] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:33] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:34] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:34] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:35] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /static/images/cense.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /static/images/IISc.png HTTP/1.1" 304 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /ports HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:36] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:37] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:38] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:38] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:38] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:39] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:39] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:39] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:39] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:40] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:41] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:41] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:41] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:42] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:42] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:42] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:42] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:10:43] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [*IDN?] -> 
 
-    def idn(self):
-        return self.query("*IDN?")
+========================
+PSU CONNECTED
+PORT: COM5
+ID: 
+========================
 
-    def set_voltage(self, voltage):
-        print(f"PSU CMD -> VSET1:{voltage}")
-        self.write(f"VSET1:{voltage}")
+127.0.0.1 - - [04/Jun/2026 16:10:45] "POST /connect_psu HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:10:47] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:10:50] "GET /data HTTP/1.1" 200 -
 
-    def set_current(self, current):
-        print(f"PSU CMD -> ISET1:{current}")
-        self.write(f"ISET1:{current}")
+Trying baudrate: 9600
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:10:52] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:10:54] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
+RAW ID RESPONSE: GWInstek,GDM8261A,GEO882227,1.02
 
-    def get_voltage(self):
-        return self.query("VOUT1?")
+================================
+DMM CONNECTED SUCCESSFULLY
+PORT: COM3
+BAUDRATE: 9600
+DMM ID: GWInstek,GDM8261A,GEO882227,1.02
+================================
 
-    def get_current(self):
-        return self.query("IOUT1?")
-
-    def output_on(self):
-        print("PSU CMD -> OUT1")
-        self.write("OUT1")
-
-    def output_off(self):
-        print("PSU CMD -> OUT0")
-        self.write("OUT0")
+127.0.0.1 - - [04/Jun/2026 16:10:57] "POST /connect_dmm HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:10:59] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:01] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:04] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [04/Jun/2026 16:11:04] "POST /dmm/start HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:06] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:08] "GET /data HTTP/1.1" 200 -
+Setting Voltage = 2.0
+Setting Current = 0.2
+PSU CMD -> VSET1:2.0
+PSU CMD -> ISET1:0.2
+RAW RESPONSE: -1.46541E-2
+RAW RESPONSE: -1.46541E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:15] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46546E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:19] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46546E-2
+RAW RESPONSE: -1.46542E-2
+RAW RESPONSE: -1.46549E-2
+RAW RESPONSE: -1.46558E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:31] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VSET1?] -> 
+VERIFY VSET = 
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:35] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46544E-2
+RAW RESPONSE: -1.46542E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:42] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46540E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:47] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:50] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:52] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:54] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46566E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:11:59] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [ISET1?] -> 
+VERIFY ISET = 
+PSU CMD -> OUT1
+PSU OUTPUT ON
+127.0.0.1 - - [04/Jun/2026 16:12:01] "POST /psu/start HTTP/1.1" 200 -
+RAW RESPONSE: -1.46557E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:06] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46560E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:10] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46554E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:15] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46548E-2
+RAW RESPONSE: -1.46552E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:21] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46548E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:26] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46555E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:30] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46555E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:35] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46548E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:39] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46560E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:44] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46547E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:49] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46549E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:53] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:12:55] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46556E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:00] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46546E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:04] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46545E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:09] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46537E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:13] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46535E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:18] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46531E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:22] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46530E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:27] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46545E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:32] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:35] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46536E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:39] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46551E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:44] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46558E-2
+RAW RESPONSE: -1.46560E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:50] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46555E-2
+RAW RESPONSE: -1.46551E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:13:57] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46551E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:02] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46545E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:07] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46554E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:12] "GET /data HTTP/1.1" 200 -
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:15] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46541E-2
+RAW RESPONSE: -1.46545E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:21] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46544E-2
+RAW RESPONSE: -1.46543E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:28] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46550E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:33] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46553E-2
+SCPI QUERY [VOUT1?] -> 
+PSU read error: could not convert string to float: ''
+127.0.0.1 - - [04/Jun/2026 16:14:37] "GET /data HTTP/1.1" 200 -
+RAW RESPONSE: -1.46547E-2
+(venv) PS D:\Wary\MEMS> 
