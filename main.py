971813b28f85d50f543aaf51a67fb94789144767
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 16:33:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import serial
-import time
-
-ser = serial.Serial(
-    port="COM5",
-    baudrate=9600,
-    timeout=2
-)
-
-time.sleep(2)
-
-commands = [
-    "VSET1:2.00\n",
-    "VSET1 2.00\n",
-    "VSET1=2.00\n"
-]
-
-for cmd in commands:
-
-    print("Sending:", cmd.strip())
-
-    ser.write(cmd.encode())
-
-    time.sleep(3)
-
-input("Check PSU display. Press ENTER...")
-
-ser.close()
+(venv) PS D:\Wary\MEMS> python test_psu_write.py
+Sending: VSET1:2.00
+Sending: VSET1 2.00
+Sending: VSET1=2.00
+Check PSU display. Press ENTER...
