c20615e8f026ea4a75786f98303fd9078ba351cb
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 14:23:42 +0530

    Remove print statements for DMM connection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from instrument import Multimeter
-
-print("Connecting to COM3...")
-
-dmm = Multimeter("COM3")
-
-print("DMM ID:")
-print(dmm.idn())
+Connecting to COM3...
+DMM ID:
+SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
+GWInstek,GDM8261A,GEO882227,1.02
+PS D:\Wary\MEMS> & d:/Wary/MEMS/venv/Scripts/python.exe d:/Wary/MEMS/test_dmm.py
+DMM ID:
+SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
+GWInstek,GDM8261A,GEO882227,1.02
+PS D:\Wary\MEMS> (Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& d:\Wary\MEMS\venv\Scripts\Activate.ps1)
