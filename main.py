076731adef770fac5b8fbbce9b16f525be8de737
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 16:28:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import serial
-import time
-
-ser = serial.Serial(
-    port="COM5",
-    baudrate=9600,
-    timeout=2
-)
-
-time.sleep(2)
-
-print("Sending 2V command...")
-
-ser.write(b"VSET1:2.00\n")
-
-time.sleep(1)
-
-ser.close()
-
-print("Done")
+(venv) PS D:\Wary\MEMS> python test_psu_write.py
+Sending 2V command...
+Done
