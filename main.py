ec3eb803e791964532010be8155b21fbd135346d
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 16:19:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-import serial
-import time
-
-ser = serial.Serial(
-    "COM5",
-    baudrate=9600,
-    timeout=2
-)
-
-time.sleep(2)
-
-ser.write(b"*IDN?\n")
-
-time.sleep(1)
-
-print(ser.readline())
-
-ser.close()
+(venv) PS D:\Wary\MEMS> python test_psu.py
+b''
