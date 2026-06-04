3348d54c0108e2158a27e91711e66a91646bd071
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 12:01:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-(venv) PS D:\Wary\MEMS> python app.py                                                                                                  
-PSU CONNECTION FAILED
-could not open port 'COM6': FileNotFoundError(2, 'The system cannot find the file specified.', None, 2)
- * Serving Flask app 'app'
- * Debug mode: off
-WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
- * Running on http://127.0.0.1:5000
-Press CTRL+C to quit
-127.0.0.1 - - [04/Jun/2026 11:59:06] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:06] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:08] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:09] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:11] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:12] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:14] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:15] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:17] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:18] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [04/Jun/2026 11:59:20] "GET /data HTTP/1.1" 200 -
+import serial.tools.list_ports
+
+for p in serial.tools.list_ports.comports():
+    print(p.device, "-", p.description)
