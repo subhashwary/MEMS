85f3a6bf4c26472f22f1e1cf4018f33ca0fa8657
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-04 17:20:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import pyvisa
-
-rm = pyvisa.ResourceManager()
-
-print("Searching...")
-
-for r in rm.list_resources():
-    print(r)
+(venv) PS D:\Wary\MEMS> python visa_scan.py
+Searching...
+D:\Wary\MEMS\venv\Lib\site-packages\pyvisa_py\tcpip.py:443: UserWarning: TCPIP:instr resource discovery is limited to the default interface.Install psutil: pip install psutil if you want to scan all interfaces.
+  warnings.warn(
+D:\Wary\MEMS\venv\Lib\site-packages\pyvisa_py\tcpip.py:122: UserWarning: TCPIP::hislip resource discovery requires the zeroconf package to be installed... try 'pip install zeroconf'
+  warnings.warn(
+ASRL1::INSTR
+ASRL3::INSTR
+ASRL5::INSTR
+TCPIP::10.56.10.35::INSTR
