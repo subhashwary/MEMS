c8733d9ed7619ccdf60b4930c9f4a6c93f318f5a
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 15:45:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-FTDIBUS\VID_0403+PID_6001+A4008SWIA\0000
+import serial
+import time
+
+ser = serial.Serial(
+    "COM5",
+    9600,
+    timeout=2
+)
+
+print("Connected to COM5")
+
+print("DTR =", ser.dtr)
+print("RTS =", ser.rts)
+
+ser.dtr = False
+ser.rts = False
+
+print("DTR OFF")
+print("RTS OFF")
+
+time.sleep(1)
+
+ser.write(b"*IDN?\r\n")
+
+time.sleep(1)
+
+print("Waiting:", ser.in_waiting)
+
+data = ser.read_all()
+
+print("Received:", data)
+
+ser.close()
