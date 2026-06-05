5747386755141c00ba66499b1ea0fa1d49042c66
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 15:56:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,11 @@
-class SCPIInstrument:
+from instrument import PowerSupply
 
-    def __init__(self, port, baudrate=115200, timeout=2):
+psu = PowerSupply("COM5", 9600)
 
-        self.port = port
+print("IDN:", psu.query("*IDN?"))
+print("VSET:", psu.query("VSET1?"))
+print("ISET:", psu.query("ISET1?"))
+print("VOUT:", psu.query("VOUT1?"))
+print("IOUT:", psu.query("IOUT1?"))
 
-        self.ser = serial.Serial(
-            port=port,
-            baudrate=baudrate,
-            timeout=timeout
-        )
-
-        self.ser.dtr = False
-        self.ser.rts = False
-
-        time.sleep(1)
-
-    def write(self, cmd):
-
-        if not cmd.endswith("\r\n"):
-            cmd += "\r\n"
-
-        self.ser.write(cmd.encode())
-
-    def query(self, cmd):
-
-        with serial_lock:
-
-            try:
-
-                self.ser.reset_input_buffer()
-
-                if not cmd.endswith("\r\n"):
-                    cmd += "\r\n"
-
-                self.ser.write(cmd.encode())
-
-                time.sleep(0.5)
-
-                raw = self.ser.read_all()
-
-                print("RAW BYTES =", raw)
-
-                response = raw.decode(
-                    errors="ignore"
-                ).strip()
-
-                print(
-                    f"SCPI QUERY [{cmd.strip()}] -> {response}"
-                )
-
-                return response
-
-            except Exception as e:
-
-                print("SCPI Query Error:", e)
-
-                return "ERROR"
-
-    def close(self):
-
-        if self.ser.is_open:
-            self.ser.close()
+psu.close()
