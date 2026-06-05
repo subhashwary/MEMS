e616d4ee26e3c6c9de6526b94816fe5e7bb57846
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 10:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-try:
-    psu = PowerSupply(
-        port="COM5",
-        baudrate=9600,
-        timeout=2
-    )
+@app.route('/psu/set', methods=['POST'])
+def set_psu():
 
-    print("================================")
-    print("PSU CONNECTED")
-    print("PORT = COM5")
-    print("================================")
+    global psu
 
-except Exception as e:
+    data = request.json
 
-    print("PSU CONNECTION FAILED")
-    print(e)
+    voltage = float(data["voltage"])
+    current = float(data["current"])
 
-    psu = None
+    try:
 
-dmm = None
+        psu.set_voltage(voltage)
+        psu.set_current(current)
+
+        print(f"SET V={voltage}")
+        print(f"SET I={current}")
+
+        return jsonify({
+            "status": "updated"
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "error": str(e)
+        }), 500
