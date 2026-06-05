3d4658fb6c76a04c8ed49c51a353665c8a99bbb1
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 15:18:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-import serial
-import time
-
-ser = serial.Serial("COM5", 9600, timeout=2)
-
-commands = [
-    "VSET1:5.0\n",
-    "ISET1:0.5\n",
-    "OUT1\n"
-]
-
-for cmd in commands:
-
-    print("Sending:", cmd.strip())
-
-    ser.write(cmd.encode())
-
-    time.sleep(1)
-
-    print("Waiting:", ser.in_waiting)
-
-    if ser.in_waiting:
-        print("Response:", ser.read_all())
-
-ser.close()
+(venv) PS D:\Wary\MEMS> python protocol_test.py
+Sending: VSET1:5.0
+Waiting: 0
+Sending: ISET1:0.5
+Waiting: 0
+Sending: OUT1
+Waiting: 0
