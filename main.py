327fe9d3c54127e416e785b25a0a2a6269ef40c3
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 17:39:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,353 +1,71 @@
-(venv) PS D:\Wary\MEMS> python app.py
- * Serving Flask app 'app'
- * Debug mode: off
-WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
- * Running on http://127.0.0.1:5000
-Press CTRL+C to quit
-127.0.0.1 - - [05/Jun/2026 17:02:46] "GET /data HTTP/1.1" 200 -
-Trying PSU baudrate: 9600
-127.0.0.1 - - [05/Jun/2026 17:03:10] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:10] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:11] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:12] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:03:13] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:13] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:14] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:15] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:03:16] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:16] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:17] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:03:18] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:19] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:19] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
 
-Trying PSU baudrate: 19200
-127.0.0.1 - - [05/Jun/2026 17:03:20] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:03:25] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:25] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:03:26] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:03:29] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
+Copyright (C) Microsoft Corporation. All rights reserved.
 
-Trying PSU baudrate: 38400
-127.0.0.1 - - [05/Jun/2026 17:03:32] "GET /data HTTP/1.1" 200 -
+Try the new cross-platform PowerShell https://aka.ms/pscore6
 
-Trying baudrate: 9600
-FAILED at 9600: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
+PS C:\Users\mahim> mode
 
-Trying baudrate: 19200
-FAILED at 19200: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
+Status for device LPT1:
+-----------------------
+    Printer output is not being rerouted.
 
-Trying baudrate: 38400
-FAILED at 38400: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
 
-Trying baudrate: 57600
-FAILED at 57600: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
+Status for device COM1:
+-----------------------
+    Baud:            1200
+    Parity:          None
+    Data Bits:       7
+    Stop Bits:       1
+    Timeout:         OFF
+    XON/XOFF:        OFF
+    CTS handshaking: OFF
+    DSR handshaking: OFF
+    DSR sensitivity: OFF
+    DTR circuit:     ON
+    RTS circuit:     ON
 
-Trying baudrate: 115200
-FAILED at 115200: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
-127.0.0.1 - - [05/Jun/2026 17:03:33] "POST /connect_dmm HTTP/1.1" 500 -
-127.0.0.1 - - [05/Jun/2026 17:03:34] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:03:35] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:37] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:03:38] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:03:40] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:41] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
 
-Trying PSU baudrate: 57600
-127.0.0.1 - - [05/Jun/2026 17:03:43] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:44] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:46] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:03:47] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:03:49] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:50] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:03:52] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:53] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-127.0.0.1 - - [05/Jun/2026 17:03:54] "POST /connect_psu HTTP/1.1" 500 -
+Status for device COM3:
+-----------------------
+    Baud:            9600
+    Parity:          None
+    Data Bits:       8
+    Stop Bits:       1
+    Timeout:         ON
+    XON/XOFF:        ON
+    CTS handshaking: OFF
+    DSR handshaking: OFF
+    DSR sensitivity: OFF
+    DTR circuit:     ON
+    RTS circuit:     ON
 
-Trying PSU baudrate: 9600
-127.0.0.1 - - [05/Jun/2026 17:03:55] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:56] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:03:58] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:03:59] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:04:01] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:04:02] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:04] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
 
-Trying PSU baudrate: 19200
-127.0.0.1 - - [05/Jun/2026 17:04:05] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:07] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:04:08] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:10] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:04:11] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:13] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:04:14] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-127.0.0.1 - - [05/Jun/2026 17:04:16] "GET /data HTTP/1.1" 200 -
+Status for device COM5:
+-----------------------
+    Baud:            115200
+    Parity:          None
+    Data Bits:       8
+    Stop Bits:       1
+    Timeout:         ON
+    XON/XOFF:        ON
+    CTS handshaking: OFF
+    DSR handshaking: OFF
+    DSR sensitivity: OFF
+    DTR circuit:     ON
+    RTS circuit:     ON
 
-Trying PSU baudrate: 38400
-127.0.0.1 - - [05/Jun/2026 17:04:17] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:19] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:04:20] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:22] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:04:23] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:04:25] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:26] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
 
-Trying PSU baudrate: 57600
-127.0.0.1 - - [05/Jun/2026 17:04:28] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:29] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [05/Jun/2026 17:04:31] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:32] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [05/Jun/2026 17:04:34] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:35] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [05/Jun/2026 17:04:37] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-127.0.0.1 - - [05/Jun/2026 17:04:38] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:04:38] "POST /connect_psu HTTP/1.1" 500 -
-127.0.0.1 - - [05/Jun/2026 17:04:38] "GET /data HTTP/1.1" 200 -
+Status for device CON:
+----------------------
+    Lines:          3000
+    Columns:        120
+    Keyboard rate:  31
+    Keyboard delay: 1
+    Code page:      437
 
-Trying baudrate: 9600
-127.0.0.1 - - [05/Jun/2026 17:04:58] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-RAW ID RESPONSE: 
-Invalid response
-
-Trying baudrate: 19200
-127.0.0.1 - - [05/Jun/2026 17:05:02] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:05:03] "GET /data HTTP/1.1" 200 -
-
-Trying baudrate: 9600
-127.0.0.1 - - [05/Jun/2026 17:05:04] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-RAW ID RESPONSE: 
-Invalid response
-
-Trying baudrate: 38400
-127.0.0.1 - - [05/Jun/2026 17:05:07] "GET /data HTTP/1.1" 200 -
-HEX = 4757496e7374656b2c47444d38323631412c47454f3838323232372c312e30320d
-RAW BYTES = b'GWInstek,GDM8261A,GEO882227,1.02\r'
-SCPI QUERY [*IDN?] -> GWInstek,GDM8261A,GEO882227,1.02
-RAW ID RESPONSE: GWInstek,GDM8261A,GEO882227,1.02
-
-================================
-DMM CONNECTED SUCCESSFULLY
-PORT: COM3
-BAUDRATE: 9600
-DMM ID: GWInstek,GDM8261A,GEO882227,1.02
-================================
-
-127.0.0.1 - - [05/Jun/2026 17:05:08] "POST /connect_dmm HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:05:10] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-RAW ID RESPONSE: 
-Invalid response
-
-Trying baudrate: 57600
-127.0.0.1 - - [05/Jun/2026 17:05:11] "GET /data HTTP/1.1" 200 -
-
-Trying PSU baudrate: 9600
-Failed at 9600: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
-
-Trying PSU baudrate: 19200
-Failed at 19200: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
-
-Trying PSU baudrate: 38400
-Failed at 38400: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
-
-Trying PSU baudrate: 57600
-Failed at 57600: could not open port 'COM5': PermissionError(13, 'Access is denied.', None, 5)
-127.0.0.1 - - [05/Jun/2026 17:05:12] "POST /connect_psu HTTP/1.1" 500 -
-127.0.0.1 - - [05/Jun/2026 17:05:13] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:05:14] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-RAW ID RESPONSE: 
-Invalid response
-
-Trying baudrate: 115200
-127.0.0.1 - - [05/Jun/2026 17:05:16] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:05:17] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:05:19] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-RAW ID RESPONSE: 
-Invalid response
-127.0.0.1 - - [05/Jun/2026 17:05:20] "POST /connect_dmm HTTP/1.1" 500 -
-127.0.0.1 - - [05/Jun/2026 17:05:20] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:07:40] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:10:51] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:10:53] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:10:54] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:10:56] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:10:57] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:18:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:19:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:19:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:19:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:20:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:20:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:20:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:21:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:21:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:21:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:22:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:22:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:22:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:23:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:23:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:23:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:24:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:24:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:24:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:25:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:25:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:25:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:26:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:26:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:26:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:27:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:27:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:27:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:28:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:28:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:28:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:29:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:29:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:29:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:30:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:30:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:30:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:31:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:31:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:31:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:32:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:32:46] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [05/Jun/2026 17:32:46] "GET /data HTTP/1.1" 200 -
-(venv) PS D:\Wary\MEMS> 
+PS C:\Users\mahim> python -m serial.tools.list_ports
+COM1
+COM3
+COM5
+3 ports found
+PS C:\Users\mahim>
