a909148347635eb97287577deed2ca8f5b747abd
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 11:06:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-<div class="ps-row-bottom">
+    <div class="ps-head" style="margin-bottom:20px;">
+        CH1
+    </div>
 
-    <button
-        class="control-btn"
-        onclick="updatePSU()">
+    <div class="instrument-row">
+        <span class="instrument-label">Set Voltage</span>
 
-        Send To PSU
+        <input
+            id="psVoltage"
+            type="number"
+            step="0.01"
+            value="2.00">
 
-    </button>
+        <span>Volt</span>
+    </div>
+
+    <div class="instrument-row">
+        <span class="instrument-label">Set Current</span>
+
+        <input
+            id="psCurrent"
+            type="number"
+            step="0.01"
+            value="0.20">
+
+        <span>Amp</span>
+    </div>
+
+    <div class="ps-row-bottom">
+        <button
+            class="control-btn start-btn"
+            onclick="startPSU()">
+            ON
+        </button>
+
+        <button
+            class="control-btn stop-btn"
+            onclick="stopPSU()">
+            OFF
+        </button>
+    </div>
 
 </div>
+
+<div class="ess-box instrument-box dmm-box">
+
+    <div class="dmm-title">Digital Multimeter</div>
+
