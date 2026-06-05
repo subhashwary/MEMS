deaa17965d039ac5ac5d393d0714be87a593e488
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 10:10:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pyvisa
-
-rm = pyvisa.ResourceManager()
-
-psu = rm.open_resource("ASRL5::INSTR")
+PS D:\Wary\MEMS> python app.py               
+Traceback (most recent call last):
+  File "D:\Wary\MEMS\app.py", line 1, in <module>
+    from flask import Flask, jsonify, request, render_template, send_file
+ModuleNotFoundError: No module named 'flask'
+PS D:\Wary\MEMS>
