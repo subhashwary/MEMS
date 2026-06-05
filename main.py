539393f88fa80cdd711ad3c366c43dc22d2ebda7
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 11:08:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 @app.route('/psu/set', methods=['POST'])
 def set_psu():
 
+    global psu
+
     data = request.json
 
     voltage = float(data["voltage"])
     current = float(data["current"])
 
-    print()
-    print("===============")
-    print("USER SET PSU")
-    print("Voltage:", voltage)
-    print("Current:", current)
-    print("===============")
+    try:
 
-    psu.set_voltage(voltage)
-    psu.set_current(current)
+        psu.set_voltage(voltage)
+        psu.set_current(current)
 
-    return jsonify({
-        "status": "ok"
-    })
+        print(f"SET V={voltage}")
+        print(f"SET I={current}")
+
+        return jsonify({
+            "status": "updated"
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "error": str(e)
+        }), 500
