abd0447715aa0adb191e7974b42827bcf84f0dc7
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 17:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import serial.tools.list_ports
-
-ports = serial.tools.list_ports.comports()
-
-for p in ports:
-    print(p.device, "-", p.description)
+(venv) PS D:\Wary\MEMS> python test_psu.py
+COM5 - USB Serial Port (COM5)
+COM1 - Communications Port (COM1)
+COM3 - USB Serial Device (COM3)
