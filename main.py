7362dab6048d950b2614f783541ab8a7aab6a233
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 14:59:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-(venv) PS D:\Wary\MEMS> python test_query.py
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN: 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET: 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-VOUT: 
-RAW BYTES = b''
-SCPI QUERY [STATUS?] -> 
-STATUS: 
+from instrument import PowerSupply
+
+psu = PowerSupply("COM5", 9600)
+
+psu.write("VSET1:3.0")
+print("sent")
+
+input("Check PSU display and press Enter...")
