4a90e0d1acce5a0ef01aa07fcec0be3a465f08e1
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 17:01:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("HEX =", raw.hex())
-print("RAW BYTES =", raw)
+@app.route('/psu_test')
+def psu_test():
+
+    global psu
+
+    try:
+        psu.write("VSET1:1.00")
+        time.sleep(1)
+
+        psu.write("ISET1:0.10")
+        time.sleep(1)
+
+        psu.write("OUT1")
+
+        return "Commands Sent"
+
+    except Exception as e:
+        return str(e)
