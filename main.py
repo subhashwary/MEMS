29e2cb99bc1bf222065ba8fe1823b713f9bcd09a
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 15:05:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,35 @@ Status for device COM5:
     DSR sensitivity: OFF
     DTR circuit:     ON
     RTS circuit:     ON
+
+
+import serial
+import time
+
+for baud in [9600, 19200, 38400, 57600, 115200]:
+
+    print("\nTesting", baud)
+
+    try:
+
+        ser = serial.Serial(
+            "COM5",
+            baudrate=baud,
+            timeout=2
+        )
+
+        time.sleep(1)
+
+        ser.write(b"*IDN?\n")
+
+        time.sleep(1)
+
+        print("Waiting:", ser.in_waiting)
+
+        print("Received:", ser.read_all())
+
+        ser.close()
+
+    except Exception as e:
+
+        print(e)
