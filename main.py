bcea05f5273032f6965fe4d562e6e3ac9e0bf426
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-05 15:47:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-import serial
-import time
-
-ser = serial.Serial(
-    "COM5",
-    9600,
-    timeout=2
-)
-
-print("Connected to COM5")
-
-print("DTR =", ser.dtr)
-print("RTS =", ser.rts)
-
-ser.dtr = False
-ser.rts = False
-
-print("DTR OFF")
-print("RTS OFF")
-
-time.sleep(1)
-
-ser.write(b"*IDN?\r\n")
-
-time.sleep(1)
-
-print("Waiting:", ser.in_waiting)
-
-data = ser.read_all()
-
-print("Received:", data)
-
-ser.close()
+(venv) PS D:\Wary\MEMS> python test_dtr.py
+Connected to COM5
+DTR = True
+RTS = True
+DTR OFF
+RTS OFF
+Waiting: 39
+Received: b'GW INSTEK,GPD-2303S,SN:EL863672,V2.04\r\n'
