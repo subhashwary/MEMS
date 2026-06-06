2de5415eca56ed39f307036e2d5ad2b79efeecea
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-06 12:40:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import re
+v = psu.query("VOUT1?")
+i = psu.query("IOUT1?")
 
-def parse_value(x):
-    try:
-        if x is None:
-            return 0.0
-        x = str(x)
-
-        # extract number from: "1.998V", "0.700A", "+0.282E+1"
-        match = re.search(r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?", x)
-        return float(match.group()) if match else 0.0
-
-    except:
-        return 0.0
+system_state["psu_voltage"] = parse_value(v)
+system_state["psu_current"] = parse_value(i)
