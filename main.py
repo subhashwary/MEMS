862a9fb3ab5565503f10d70416b34bcdded13ab0
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-06 12:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,344 @@
-if response and "Invalid" not in response:
+(venv) PS D:\Wary\MEMS> python app.py
+ * Serving Flask app 'app'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+127.0.0.1 - - [06/Jun/2026 12:24:39] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:44] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:45] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:45] "GET /static/images/IISc.png HTTP/1.1" 304 -
+127.0.0.1 - - [06/Jun/2026 12:24:45] "GET /static/images/cense.png HTTP/1.1" 304 -
+127.0.0.1 - - [06/Jun/2026 12:24:46] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:46] "GET /ports HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:55] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:55] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:56] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:56] "GET /static/images/IISc.png HTTP/1.1" 304 -
+127.0.0.1 - - [06/Jun/2026 12:24:56] "GET /static/images/cense.png HTTP/1.1" 304 -
+127.0.0.1 - - [06/Jun/2026 12:24:57] "GET /ports HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:24:58] "GET /data HTTP/1.1" 200 -
+
+Trying PSU baudrate: 9600
+127.0.0.1 - - [06/Jun/2026 12:24:59] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:25:03] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:25:05] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:25:10] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 19200
+127.0.0.1 - - [06/Jun/2026 12:25:11] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:25:14] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:25:17] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:25:19] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 38400
+127.0.0.1 - - [06/Jun/2026 12:25:22] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:25:25] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:25:28] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:25:31] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+127.0.0.1 - - [06/Jun/2026 12:25:33] "GET /data HTTP/1.1" 200 -
+
+Trying PSU baudrate: 57600
+127.0.0.1 - - [06/Jun/2026 12:25:34] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:25:37] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:25:40] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:25:42] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+127.0.0.1 - - [06/Jun/2026 12:25:44] "POST /connect_psu HTTP/1.1" 500 -
+
+Trying PSU baudrate: 9600
+127.0.0.1 - - [06/Jun/2026 12:25:44] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:25:48] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:25:50] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:25:53] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 19200
+127.0.0.1 - - [06/Jun/2026 12:25:55] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:00] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:26:01] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:26:01] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:02] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:26:04] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 38400
+127.0.0.1 - - [06/Jun/2026 12:26:07] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:26:07] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:26:08] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:10] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:12] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:26:13] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:26:16] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 57600
+127.0.0.1 - - [06/Jun/2026 12:26:18] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:22] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:25] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:26:26] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+127.0.0.1 - - [06/Jun/2026 12:26:28] "POST /connect_psu HTTP/1.1" 500 -
+
+Trying PSU baudrate: 9600
+127.0.0.1 - - [06/Jun/2026 12:26:29] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:32] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:35] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:26:38] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+127.0.0.1 - - [06/Jun/2026 12:26:40] "GET /data HTTP/1.1" 200 -
+
+Trying PSU baudrate: 19200
+127.0.0.1 - - [06/Jun/2026 12:26:41] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:44] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:47] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:26:49] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 38400
+127.0.0.1 - - [06/Jun/2026 12:26:52] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:26:55] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:26:58] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:27:01] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 57600
+127.0.0.1 - - [06/Jun/2026 12:27:02] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:27:07] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:27:08] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:27:11] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /connect_psu HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /psu/start HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "GET /data HTTP/1.1" 200 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "POST /psu/set HTTP/1.1" 500 -
+127.0.0.1 - - [06/Jun/2026 12:27:13] "GET /data HTTP/1.1" 200 -
+
+Trying PSU baudrate: 9600
+127.0.0.1 - - [06/Jun/2026 12:27:37] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:27:40] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:27:43] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:27:45] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN RESPONSE = 
+
+Trying PSU baudrate: 19200
+127.0.0.1 - - [06/Jun/2026 12:27:48] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [*IDN?] -> 
+IDN  = 
+127.0.0.1 - - [06/Jun/2026 12:27:51] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [VSET1?] -> 
+VSET = 
+127.0.0.1 - - [06/Jun/2026 12:27:54] "GET /data HTTP/1.1" 200 -
+HEX = 
+RAW BYTES = b''
+SCPI QUERY [ISET1?] -> 
+ISET = 
+127.0.0.1 - - [06/Jun/2026 12:27:56] "GET /data HTTP/1.1" 200 -
