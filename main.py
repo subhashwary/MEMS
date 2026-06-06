aa01797402bb9f091fcf09e1a8a4fe47477cfcfc
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-06 13:09:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-if response and "*" in response or "IDN" in response:
+@app.route('/psu/stop', methods=['POST'])
+def stop_psu():
+
+    global psu
+
+    if not psu:
+        return jsonify({"error": "PSU not connected"}), 500
+
+    try:
+        with psu_lock:
+            psu.write("OUT0\n")   # 🔴 PUT HERE (THIS IS CORRECT PLACE)
+
+        return jsonify({
+            "status": "stopped"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
