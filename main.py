9618fb2d0cc3e0ee443cbf5cd001d889117f9390
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-06 13:37:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-def send_psu(voltage, current):
-    global psu
+psu.write("VOUT1?\r\n")
+v = psu.read().strip()
 
-    if not psu:
-        return "PSU not connected"
-
-    try:
-        with psu_lock:
-
-            cmd_v = f"VSET1:{voltage:.3f}\r\n"
-            cmd_i = f"ISET1:{current:.3f}\r\n"
-
-            psu.write(cmd_v)
-            time.sleep(0.1)
-
-            psu.write(cmd_i)
-            time.sleep(0.1)
-
-            psu.write("OUT1\r\n")
-
-        return "OK"
-
-    except Exception as e:
-        return str(e)
+psu.write("IOUT1?\r\n")
+i = psu.read().strip()
