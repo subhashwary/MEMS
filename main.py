5549bafb1cb6bbc7a63bda6fe4f748b4f8578c39
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-06 16:26:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,19 @@
-(venv) PS D:\Wary\MEMS> python app.py
- * Serving Flask app 'app'
- * Debug mode: off
-WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
- * Running on http://127.0.0.1:5000
-Press CTRL+C to quit
-127.0.0.1 - - [06/Jun/2026 16:20:10] "GET / HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:10] "GET /static/images/IISc.png HTTP/1.1" 304 -
-127.0.0.1 - - [06/Jun/2026 16:20:10] "GET /static/images/cense.png HTTP/1.1" 304 -
-127.0.0.1 - - [06/Jun/2026 16:20:10] "GET /ports HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:17] "GET / HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:17] "GET /static/images/cense.png HTTP/1.1" 304 -
-127.0.0.1 - - [06/Jun/2026 16:20:17] "GET /static/images/IISc.png HTTP/1.1" 304 -
-127.0.0.1 - - [06/Jun/2026 16:20:17] "GET /ports HTTP/1.1" 200 -
+@app.route('/psu_debug')
+def psu_debug():
 
-Trying PSU baudrate: 9600
-127.0.0.1 - - [06/Jun/2026 16:20:22] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [*IDN?] -> 
-IDN  = 
-127.0.0.1 - - [06/Jun/2026 16:20:26] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VSET1?] -> 
-VSET = 
-127.0.0.1 - - [06/Jun/2026 16:20:28] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [ISET1?] -> 
-ISET = 
-127.0.0.1 - - [06/Jun/2026 16:20:31] "GET /data HTTP/1.1" 200 -
-PORT = COM5
-BAUD = 9600
-OPEN = True
+    global psu
 
-================================
-PSU CONNECTED SUCCESSFULLY
-PORT: COM5
-BAUDRATE: 9600
-================================
+    if not psu:
+        return "No PSU"
 
-127.0.0.1 - - [06/Jun/2026 16:20:32] "POST /connect_psu HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [IOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:20:38] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:42] "POST /psu/set HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:42] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [IOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:20:48] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:20:52] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [IOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:20:54] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:54] "POST /psu/set HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:54] "GET /data HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:57] "POST /psu/set HTTP/1.1" 200 -
-127.0.0.1 - - [06/Jun/2026 16:20:57] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [IOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:21:04] "GET /data HTTP/1.1" 200 -
-HEX = 
-RAW BYTES = b''
-SCPI QUERY [VOUT1?] -> 
-127.0.0.1 - - [06/Jun/2026 16:21:08] "GET /data HTTP/1.1" 200 -
+    try:
+
+        print("VSET1? =", psu.query("VSET1?"))
+        print("ISET1? =", psu.query("ISET1?"))
+        print("VOUT1? =", psu.query("VOUT1?"))
+        print("IOUT1? =", psu.query("IOUT1?"))
+
+        return "Done"
+
+    except Exception as e:
+        return str(e)
