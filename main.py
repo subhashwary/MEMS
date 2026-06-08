13496b44a7c6eb2ee54f3f552e6c8ba5e282173e
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 12:32:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,13 @@ try:
     with psu_lock:
 
         psu.write(f"VSET1:{voltage:.3f}")
+        time.sleep(0.1)
+
         psu.write(f"ISET1:{current:.3f}")
+        time.sleep(0.1)
+
         psu.write("OUT1")
-
-        system_state["psu_voltage"] = voltage
-        system_state["psu_current"] = current
-
-        psu.output_on()
-
-        time.sleep(1.5)
-
-        print("VERIFY VOUT =", psu.query("VOUT1?"))
-        print("VERIFY IOUT =", psu.query("IOUT1?"))
-
-    print("PSU OUTPUT ON")
-
-    return jsonify({
-        "status": "started"
-    })
+        time.sleep(0.1)
 
 finally:
 
