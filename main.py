7433d8977c9cefeab916b473737ec8aa1ed1a644
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 12:37:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-with psu_lock:
+except Exception as e:
 
-    psu.ser.reset_input_buffer()
+    global psu
 
-    v = psu.query("VOUT1?")
-    i = psu.query("IOUT1?")
+    print("PSU read error:", e)
 
-if not v or not i:
+    try:
+        psu.close()
+    except:
+        pass
 
-    print("WARNING: PSU returned empty response")
-
-else:
-
-    system_state["psu_voltage"] = parse_value(v)
-    system_state["psu_current"] = parse_value(i)
-
-    system_state["last_psu_read"] = time.time()
+    psu = None
