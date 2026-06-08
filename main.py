1d2eda859738ed7e63cb17b1a73dbdd5e8b9f482
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 17:38:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-(venv) PS D:\Wary\MEMS> python ports.py
+import serial
+import time
 
-===================
-Testing COM3
-Received: b''
+ser = serial.Serial(
+    "COM5",
+    baudrate=9600,
+    timeout=2
+)
 
-===================
-Testing COM5
-Received: b''
+for cmd in [
+    "VSET1:2.00\r\n",
+    "ISET1:0.10\r\n",
+    "OUT1\r\n"
+]:
+    print("Sending:", repr(cmd))
+    ser.write(cmd.encode())
+    time.sleep(1)
+
+ser.close()
+
+print("Done")
