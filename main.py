e2f30ce419b8d0a13104f84c7a573af191579e3e
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 17:31:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-(venv) PS D:\Wary\MEMS> python test_psu.py
-Opening COM5...
+import serial
+import time
 
-Sending: VSET1?
-HEX: 
-RAW: b''
+ser = serial.Serial(
+    port="COM5",
+    baudrate=9600,
+    timeout=2
+)
 
-Sending: ISET1?
-HEX: 
-RAW: b''
+time.sleep(1)
 
-Sending: STATUS?
-HEX: 
-RAW: b''
-Done
+print("Sending commands...")
+
+ser.write(b"VSET1:2.00\r\n")
+time.sleep(1)
+
+ser.write(b"ISET1:0.10\r\n")
+time.sleep(1)
+
+ser.write(b"OUT1\r\n")
+
+print("Done")
+
+ser.close()
