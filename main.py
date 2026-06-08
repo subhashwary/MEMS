c31759a0164dc60c1c4be536988577cec587bf07
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 17:36:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-(venv) PS D:\Wary\MEMS> python test_psu_write.py
-Sending commands...
-Done
-(venv) PS D:\Wary\MEMS> 
+import serial
+import time
+
+for port in ["COM3", "COM5"]:
+
+    print("\n===================")
+    print("Testing", port)
+
+    try:
+        ser = serial.Serial(
+            port,
+            baudrate=9600,
+            timeout=2
+        )
+
+        ser.write(b"VSET1?\n")
+
+        time.sleep(1)
+
+        data = ser.read(100)
+
+        print("Received:", data)
+
+        ser.close()
+
+    except Exception as e:
+        print(e)
