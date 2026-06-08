dd91bed45781459398c06daab3a8ffc7f7401418
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 17:37:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-import serial
-import time
+(venv) PS D:\Wary\MEMS> python ports.py
 
-for port in ["COM3", "COM5"]:
+===================
+Testing COM3
+Received: b''
 
-    print("\n===================")
-    print("Testing", port)
-
-    try:
-        ser = serial.Serial(
-            port,
-            baudrate=9600,
-            timeout=2
-        )
-
-        ser.write(b"VSET1?\n")
-
-        time.sleep(1)
-
-        data = ser.read(100)
-
-        print("Received:", data)
-
-        ser.close()
-
-    except Exception as e:
-        print(e)
+===================
+Testing COM5
+Received: b''
