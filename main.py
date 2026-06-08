d76f0c11a4e000fa87ddc49daa4faa00398aced3
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-08 17:29:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-(venv) PS D:\Wary\MEMS> python test_psu.py
-COM5 - USB Serial Port (COM5)
-COM1 - Communications Port (COM1)
-COM3 - USB Serial Device (COM3)
+import serial
+import time
+
+print("Opening COM5...")
+
+ser = serial.Serial(
+    port="COM5",
+    baudrate=9600,
+    timeout=2
+)
+
+time.sleep(1)
+
+for cmd in ["VSET1?", "ISET1?", "STATUS?"]:
+
+    print("\nSending:", cmd)
+
+    ser.reset_input_buffer()
+
+    ser.write((cmd + "\r\n").encode())
+
+    time.sleep(1)
+
+    data = ser.read(200)
+
+    print("HEX:", data.hex())
+    print("RAW:", data)
+
+ser.close()
+
+print("Done")
