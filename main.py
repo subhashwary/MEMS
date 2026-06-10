43d625ccc3b217f2a2765955ea173b13fd966016
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 14:53:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-<div class="card">
-
-    <h3>Current Event</h3>
-
-    <div id="cycleEvent">
-
-        Waiting
-
-    </div>
-
-</div>
+document.getElementById("cycleEvent")
+.innerText =
+    data.cycle_event;
