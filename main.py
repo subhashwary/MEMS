8fa352f648f824cad7bb282dc42194474717314e
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 12:05:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-<input
-    id="initialDelay"
-    type="number"
-    value="10">
+function sendCycleConfig() {
+
+    const config = {
+
+        initial_off:
+            document.getElementById(
+                "initialDelay"
+            ).value,
+
+        on_time:
+            document.getElementById(
+                "onTime"
+            ).value,
+
+        off_time:
+            document.getElementById(
+                "offTime"
+            ).value,
+
+        cycles:
+            document.getElementById(
+                "cycleCountInput"
+            ).value
+    };
+
+    return fetch('/config', {
+
+        method:'POST',
+
+        headers:{
+            'Content-Type':'application/json'
+        },
+
+        body:JSON.stringify(config)
+    });
+}
