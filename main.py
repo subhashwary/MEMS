cb65ee1a2f04d9944ec29b7f9b8df966e3e3fbd7
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 14:23:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-"current_cycle":
-    system_state["current_cycle"],
+.ess-idle {
+    color: gray;
+    font-weight: bold;
+}
 
-"ess_state":
-    system_state["ess_state"],
+.ess-delay {
+    color: blue;
+    font-weight: bold;
+}
 
-"cycle_event":
-    system_state["cycle_event"],
+.ess-on {
+    color: green;
+    font-weight: bold;
+}
+
+.ess-off {
+    color: orange;
+    font-weight: bold;
+}
+
+.ess-complete {
+    color: darkgreen;
+    font-weight: bold;
+}
+
+.ess-stop {
+    color: red;
+    font-weight: bold;
+}
