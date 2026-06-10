1722a3f2efad9465be393b344547dc7d7600300f
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 11:11:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-"ess_state": "IDLE",
+@app.route('/auto/start', methods=['POST'])
+def auto_start():
 
-"current_cycle": 0,
+    system_state["mode"] = "auto"
 
-"auto_running": False,
+    system_state["cycle_start"] = time.time()
 
-"cycle_event": "",
+    system_state["auto_running"] = True
+
+    system_state["current_cycle"] = 0
+
+    system_state["ess_state"] = "INITIAL_DELAY"
+
+    return jsonify({
+        "status":"started"
+    })
+
+
+@app.route('/auto/stop', methods=['POST'])
+def auto_stop():
+
+    global psu
+
+    system_state["auto_running"] = False
+
+    system_state["ess_state"] = "STOPPED"
+
+    system_state["dmm_running"] = False
+
+    if psu:
+
+        try:
+            with psu_lock:
+                psu.write("OUT0")
+        except:
+            pass
+
+    system_state["psu_output"] = False
+
+    return jsonify({
+        "status":"stopped"
+    })
