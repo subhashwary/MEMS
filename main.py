775779105fc064d66493ca426f084cea46f7f603
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 17:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,4 @@
-if (data.mode === "auto") {
-
-    // DMM
-    dmmStartBtn.disabled = true;
-    dmmStopBtn.disabled = true;
-
-    dmmStartBtn.style.opacity = 0.5;
-    dmmStopBtn.style.opacity = 0.5;
-
-    // PSU
-    psuSendBtn.disabled = true;
-    psuOnBtn.disabled = true;
-    psuOffBtn.disabled = true;
-
-    psuSendBtn.style.opacity = 0.5;
-    psuOnBtn.style.opacity = 0.5;
-    psuOffBtn.style.opacity = 0.5;
-
-    // Inputs
-    voltageInput.disabled = true;
-    currentInput.disabled = true;
-
-}
-else {
-
-    // DMM
-    dmmStartBtn.disabled = false;
-    dmmStopBtn.disabled = false;
-
-    dmmStartBtn.style.opacity = 1;
-    dmmStopBtn.style.opacity = 1;
-
-    // PSU
-    psuSendBtn.disabled = false;
-    psuOnBtn.disabled = false;
-    psuOffBtn.disabled = false;
-
-    psuSendBtn.style.opacity = 1;
-    psuOnBtn.style.opacity = 1;
-    psuOffBtn.style.opacity = 1;
-
-    // Inputs
-    voltageInput.disabled = false;
-    currentInput.disabled = false;
-}
+if system_state["mode"] == "auto":
+    return jsonify({
+        "error": "Manual PSU control disabled in AUTO mode"
+    }), 403
