a30944c1cf32445aa525c458ce1de853c39efcca
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 14:41:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-if (
-    system_state["mode"] == "auto"
-    and system_state["auto_running"]
-):
-
-    cfg = system_state["config"]
-
-    elapsed = time.time() - system_state["cycle_start"]
-
-    initial_delay = cfg["initial_off"]
-    on_time = cfg["on_time"]
-    off_time = cfg["off_time"]
-    cycles = cfg["cycles"]
-
-    cycle_period = on_time + off_time
+system_state["cycle_event"] = \
+    "Waiting Initial Delay"
