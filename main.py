80de53ac381bb5161b2e19773e4e0e56a5dd1546
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-10 14:30:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-const essEl =
-    document.getElementById("essState");
-
-essEl.className = "";
-
-if(data.ess_state === "INITIAL_DELAY")
-{
-    essEl.classList.add("ess-delay");
-}
-else if(data.ess_state.includes("_ON"))
-{
-    essEl.classList.add("ess-on");
-}
-else if(data.ess_state.includes("_OFF"))
-{
-    essEl.classList.add("ess-off");
-}
-else if(data.ess_state === "COMPLETE")
-{
-    essEl.classList.add("ess-complete");
-}
-else if(data.ess_state === "STOPPED")
-{
-    essEl.classList.add("ess-stop");
-}
-else
-{
-    essEl.classList.add("ess-idle");
-}
+Timestamp
+Pressure
+DMM_Voltage
+PSU_Voltage
+PSU_Current
+Mode
+ESS_State
+Cycle
+Event
