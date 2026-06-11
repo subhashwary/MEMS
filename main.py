56aa394df7e35babc69264dbce507485a5a72855
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-11 15:44:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
-fetch('/mode',{
-    method:'POST',
-    headers:{
-        'Content-Type':'application/json'
-    },
-    body:JSON.stringify({
-        mode:'auto'
+function startAuto() {
+
+    fetch('/mode', {
+
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            mode: 'auto'
+        })
+
     })
-})
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data);
+
+        alert("ESS Cycle Started");
+
+    });
+
+}
+
+function stopAuto() {
+
+    fetch('/mode', {
+
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            mode: 'manual'
+        })
+
+    })
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data);
+
+        alert("ESS Cycle Stopped");
+
+    });
+
+}
