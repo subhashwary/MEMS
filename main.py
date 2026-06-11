f8246e0981566cd162d13d130dd1efb7ee7675ab
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-11 10:18:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("AUTO STARTED")
-print("elapsed =", elapsed)
-print("adjusted =", adjusted)
+elif mode == "auto":
+
+    system_state["mode"] = "auto"
+    system_state["auto_running"] = True
+
+    system_state["cycle_start"] = time.time()
+
+    system_state["current_cycle"] = 0
+    system_state["ess_state"] = "INITIAL_DELAY"
+    system_state["cycle_event"] = "Starting ESS"
+
+    return jsonify({
+        "status":"auto mode"
+    })
