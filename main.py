f8105ab0f20a4fc43bd7a1de95c16e2645f92c6c
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-11 17:14:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-<div class="ess-box instrument-box status-box" style="grid-column:1;">
-
-    <h3>System Status</h3>
-
-    <div>
-        PSU Connection:
-        <span id="psuStatus">🔴</span>
-    </div>
-
-    <br>
-
-    <div>
-        DMM Connection:
-        <span id="dmmStatus">🔴</span>
-    </div>
-
-    <br>
-
-    <div>
-        PSU Output:
-        <span id="psuOutputStatus">🔴</span>
-    </div>
-
-    <br>
-
-    <div>
-        Auto Mode:
-        <span id="autoStatus">🔴</span>
-    </div>
-
-</div>
+.status-box {
+    min-height: 420px;
+    justify-content: flex-start;
+    align-items: flex-start;
+    text-align: left;
+}
