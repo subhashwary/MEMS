b2457122a8fbea8289177bd04ddd3d748a34492d
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-11 10:20:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-elif mode == "auto":
+if completed_cycles >= cycles:
 
-    system_state["mode"] = "auto"
-    system_state["auto_running"] = True
+    system_state["auto_running"] = False
+    system_state["mode"] = "manual"
 
-    system_state["cycle_start"] = time.time()
-
-    system_state["current_cycle"] = 0
-    system_state["ess_state"] = "INITIAL_DELAY"
-    system_state["cycle_event"] = "Starting ESS"
-
-    return jsonify({
-        "status":"auto mode"
-    })
+    system_state["ess_state"] = "COMPLETE"
+    system_state["cycle_event"] = "ESS Completed"
