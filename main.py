19f91be0a1c5f1e75feba08d8e8757fb043cad3f
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-11 17:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-.status-box {
-    min-height: 420px;
-    justify-content: flex-start;
-    align-items: flex-start;
-    text-align: left;
-}
+<div class="card">
+
+    <h3>ESS Status</h3>
+
+    <div id="essState">
+        IDLE
+    </div>
+
+    <br>
+
+    <div id="cycleCount">
+        Cycle 0
+    </div>
+
+</div>
+
+<div class="card">
+
+    <h3>Current Event</h3>
+
+    <div id="cycleEvent">
+        Waiting...
+    </div>
+
+</div>
