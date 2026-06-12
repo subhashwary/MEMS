2df53f8160b2fbecb1dc494efec21e79ad8b158c
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-12 11:51:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-<div class="ess-box instrument-box status-box" 
-    style="grid-column: 1;">
-
-    <h3>System Status</h3>
-    <div>
-        PSU Connection:
-        <span id="psuStatus">🔴</span>
-    </div>
-    <br>
-    <div>
-        DMM Connection:
-        <span id="dmmStatus">🔴</span>
-    </div>
-    <br>
-    <div>
-        PSU Output:
-        <span id="psuOutputStatus">🔴</span>
-    </div>
-    <br>
-    <div>
-        Auto Mode:
-        <span id="autoStatus">🔴</span>
-    </div>
-
-    <h3>ESS Status</h3>
-    <div id="essState">
-        IDLE
-    </div>
-    <br>
-    <div id="cycleCount">
-        Cycle 0
-    </div>
+<div class="ps-row-bottom">
+    <button
+        id="psuSendBtn"
+        class="control-btn"
+        onclick="updatePSU()">
+        Send To PSU
+    </button>
 </div>
+
+<div class="ps-row-bottom" style="margin-top:15px;">
+    <button
+        id="psuOnBtn"
+        class="control-btn start-btn"
+        onclick="startPSU()">
+        ON
+    </button>
+
+    <button
+        id="psuOffBtn"
+        class="control-btn stop-btn"
+        onclick="stopPSU()">
+        OFF
+    </button>
+</div>
+
+</div> <!-- PSU BOX CLOSED -->
