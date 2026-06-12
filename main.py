f34c0d96fdd37506cf8c3209a2ba09a1646c46b7
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-12 12:54:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-.psu-controls {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-}
+<div class="psu-controls">
+
+    <div class="ps-row-bottom">
+        <button
+            id="psuSendBtn"
+            class="control-btn"
+            onclick="updatePSU()">
+            Send To PSU
+        </button>
+    </div>
+
+    <div class="ps-row-bottom">
+        <button
+            id="psuOnBtn"
+            class="control-btn start-btn"
+            onclick="startPSU()">
+            ON
+        </button>
+
+        <button
+            id="psuOffBtn"
+            class="control-btn stop-btn"
+            onclick="stopPSU()">
+            OFF
+        </button>
+    </div>
+
+</div>
