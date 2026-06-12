b2aa7e65ec2621a8d366fb2633c40294a7a787f3
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-12 11:46:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-.ess-grid {
-    display:grid;
-    grid-template-columns:
-        minmax(350px,1fr)
-        minmax(350px,1fr)
-        180px;
-}
+<div class="ess-box instrument-box status-box" 
+    style="grid-column: 1;">
+
+    <h3>System Status</h3>
+    <div>
+        PSU Connection:
+        <span id="psuStatus">🔴</span>
+    </div>
+    <br>
+    <div>
+        DMM Connection:
+        <span id="dmmStatus">🔴</span>
+    </div>
+    <br>
+    <div>
+        PSU Output:
+        <span id="psuOutputStatus">🔴</span>
+    </div>
+    <br>
+    <div>
+        Auto Mode:
+        <span id="autoStatus">🔴</span>
+    </div>
+
+    <h3>ESS Status</h3>
+    <div id="essState">
+        IDLE
+    </div>
+    <br>
+    <div id="cycleCount">
+        Cycle 0
+    </div>
+</div>
