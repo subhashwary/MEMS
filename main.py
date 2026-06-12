592dbb87e0fa33da5b866cc2ae311ee64c210a7c
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-12 12:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-.psu-box {
+.psu-controls {
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-}
-
-.ps-row-bottom {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    margin-top: 15px;
+    align-items: center;
     width: 100%;
 }
