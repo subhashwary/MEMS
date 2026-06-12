fbb93dfc32be6d4833f3c2c758e8b439384b9429
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-12 17:02:51 +0530

    Update an.py

--- an.py
+++ an.py
@@ -1,2 +1,20 @@
-<div class="ess-box ess-status-box"
-     style="grid-column:2;grid-row:2;">
+fetch('/ports')
+.then(res => {
+
+    if(!res.ok){
+        throw new Error("Failed to load ports");
+    }
+
+    return res.json();
+})
+.then(...)
+.catch(err => {
+
+    console.error(err);
+
+    document.getElementById("comPortSelect").innerHTML =
+        "<option>No Ports</option>";
+
+    document.getElementById("psuPortSelect").innerHTML =
+        "<option>No Ports</option>";
+});
