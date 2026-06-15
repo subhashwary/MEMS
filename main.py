945b1f4f82eb2e1e38d16f9c55efe554ccf62adc
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-15 12:33:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-const ctx = document.getElementById('chart').getContext('2d');
+function resetData() {
+
+    labels.length = 0;
+
+    pressureData.length = 0;
+    dmmData.length = 0;
+
+    psuVoltageData.length = 0;
+    psuCurrentData.length = 0;
+
+    essData.length = 0;
+    delayData.length = 0;
+
+    modeData.length = 0;
+    cycleData.length = 0;
+
+    chart.update();
+}
