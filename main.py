e59c150d24265873303796a758434a56234c8c69
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-15 11:47:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,15 @@
-let labels = [];
+labels.push(data.timestamp);
 
-let pressureData = [];
-let dmmData = [];
-let psuVoltageData = [];
-let psuCurrentData = [];
+pressureData.push(data.pressure);
 
-let essData = [];
-let modeData = [];
-let cycleData = [];
+dmmData.push(data.dmm_voltage);
 
-const chart = new Chart(ctx, {
-    type: 'line',
+psuVoltageData.push(data.psu_voltage);
 
-    data: {
-        labels: labels,
+psuCurrentData.push(data.psu_current);
 
-        datasets: [
+essData.push(data.ess_numeric);
 
-            {
-                label: 'Pressure (kPa)',
-                data: pressureData,
-                borderWidth: 2,
-                yAxisID: 'y'
-            },
+modeData.push(data.mode_numeric);
 
-            {
-                label: 'DMM Voltage (V)',
-                data: dmmData,
-                borderWidth: 2,
-                yAxisID: 'y'
-            },
-
-            {
-                label: 'PSU Voltage (V)',
-                data: psuVoltageData,
-                borderWidth: 2,
-                yAxisID: 'y'
-            },
-
-            {
-                label: 'PSU Current (A)',
-                data: psuCurrentData,
-                borderWidth: 2,
-                yAxisID: 'y1'
-            },
-
-            {
-                label: 'ESS State',
-                data: essData,
-                stepped: true,
-                borderWidth: 3,
-                yAxisID: 'y2'
-            },
-
-            {
-                label: 'Mode',
-                data: modeData,
-                stepped: true,
-                borderWidth: 3,
-                yAxisID: 'y2'
-            },
-
-            {
-                label: 'Cycle',
-                data: cycleData,
-                borderWidth: 2,
-                yAxisID: 'y3'
-            }
-        ]
-    },
-
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-
-        scales: {
-
-            y: {
-                type: 'linear',
-                position: 'left',
-                title: {
-                    display: true,
-                    text: 'Voltage / Pressure'
-                }
-            },
-
-            y1: {
-                type: 'linear',
-                position: 'right',
-                title: {
-                    display: true,
-                    text: 'Current (A)'
-                },
-
-                grid: {
-                    drawOnChartArea: false
-                }
-            },
-
-            y2: {
-                min: 0,
-                max: 2,
-
-                position: 'right',
-
-                title: {
-                    display: true,
-                    text: 'State'
-                },
-
-                grid: {
-                    drawOnChartArea: false
-                }
-            },
-
-            y3: {
-                position: 'right',
-
-                title: {
-                    display: true,
-                    text: 'Cycle'
-                },
-
-                grid: {
-                    drawOnChartArea: false
-                }
-            }
-        }
-    }
-});
+cycleData.push(data.current_cycle);
