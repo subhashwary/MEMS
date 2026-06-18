0018ef0c6a7cde332e44cbeedb48b53bede1ebb5
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-18 12:27:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-if (ports.length === 0) {
+initial_off = max(1, int(data.get("initial_off", 5)))
+on_time = max(1, int(data.get("on_time", 5)))
+off_time = max(1, int(data.get("off_time", 5)))
+cycles = max(1, int(data.get("cycles", 10)))
 
-            dmmSelect.innerHTML =
-                '<option>No Ports Found</option>';
-
-            psuSelect.innerHTML =
-                '<option>No Ports Found</option>';
-
-            return;
-        }
+system_state["config"] = {
+    "initial_off": initial_off,
+    "on_time": on_time,
+    "off_time": off_time,
+    "cycles": cycles
+}
