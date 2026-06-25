0285c7cb8e06882c16abdf4bac4f2e9a2953caaf
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-25 15:13:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-import csv
-import os
-from datetime import datetime
-
-CSV_FILE = "sensor_data.csv"
-
 def initialize_csv():
-    if not os.path.exists(CSV_FILE):
-        with open(CSV_FILE, "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                "Timestamp",
-                "DMM_Voltage",
-                "PSU_Voltage",
-                "Mode",
-                "ESS_State",
-                "Cycle",
-                "Event"
-            ])
 
-def log_data(
-    dmm_voltage,
-    psu_voltage,
-    mode,
-    ess_state,
-    cycle,
-    event
-):
-    with open(CSV_FILE, "a", newline="") as f:
+    with open(CSV_FILE, "w", newline="") as f:
 
         writer = csv.writer(f)
 
         writer.writerow([
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            dmm_voltage,
-            psu_voltage,
-            mode,
-            ess_state,
-            cycle,
-            event
+            "Timestamp",
+            "DMM_Voltage",
+            "PSU_Voltage",
+            "Mode",
+            "ESS_State",
+            "Cycle",
+            "Event"
         ])
