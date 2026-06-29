336790535f8fe4d4abc2e5ab14e578f28d8d1d2e
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-29 17:06:38 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,24 +1,4 @@
-function updateControls(mode) {
+manualBtn.classList.toggle("active", data.mode === "manual");
+autoBtn.classList.toggle("active", data.mode === "auto");
 
-    const auto = mode === "auto";
-
-    // DMM
-    dmmStartBtn.disabled = auto;
-    dmmStopBtn.disabled = auto;
-
-    dmmStartBtn.style.opacity = auto ? 0.5 : 1;
-    dmmStopBtn.style.opacity = auto ? 0.5 : 1;
-
-    // PSU
-    psuSendBtn.disabled = auto;
-    psuOnBtn.disabled = auto;
-    psuOffBtn.disabled = auto;
-
-    psuSendBtn.style.opacity = auto ? 0.5 : 1;
-    psuOnBtn.style.opacity = auto ? 0.5 : 1;
-    psuOffBtn.style.opacity = auto ? 0.5 : 1;
-
-    // Inputs
-    voltageInput.disabled = auto;
-    currentInput.disabled = auto;
-}
+updateControls(data.mode);
