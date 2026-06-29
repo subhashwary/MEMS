53c33d23f3938df2627501675588547eb3c5249d
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-29 14:03:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-@app.route('/reset', methods=['POST'])
-def reset():
-
-    global system_state
-
-    system_state["mode"] = "manual"
-    system_state["auto_running"] = False
-    system_state["dmm_running"] = False
-
-    system_state["current_cycle"] = 0
-    system_state["cycle_event"] = "Waiting"
-    system_state["ess_state"] = "IDLE"
-
-    system_state["dmm_voltage"] = 0.0
-    system_state["psu_voltage"] = 0.0
-    system_state["psu_current"] = 0.0
-    system_state["pressure"] = 0.0
-
-    system_state["cycle_start"] = time.time()
-
-    # Erase old CSV and create fresh one
-    initialize_csv()
-
-    return jsonify({
-        "status": "reset complete"
-    })
+datasets: [
+    {
+        label: 'DMM Voltage (V)',
+        data: dmmData,
+        borderColor: 'green',
+        borderWidth: 2,
+        yAxisID: 'y',
+        fill: false,
+        tension: 0.2
+    }
+]
