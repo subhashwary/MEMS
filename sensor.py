0e11f70f7ce2de9481ab4ca1817b982ccbf2c8c3
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-29 17:16:11 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,31 +1,22 @@
-const chart = new Chart(ctx, {
-
-    type: 'line',
-
-    data: {
-        labels: labels,
-
-        datasets: [
-
-            {
-                label: 'DMM Voltage (V)',
-                data: dmmData,
-                borderColor: 'green',
-                borderWidth: 2,
-                yAxisID: 'y',
-                fill: false,
-                tension: 0.2
-            },
-
-            {
-                label: 'PSU Output',
-                data: psuStateData,
-                stepped: true,
-                borderColor: 'red',
-                borderWidth: 2,
-                pointRadius: 0,
-                yAxisID: 'y1'
-            }
-
-        ]
+y: {
+    title: {
+        display: true,
+        text: 'Voltage (V)'
     },
+    min: 0,
+    max: 3
+},
+
+y1: {
+    position: 'right',
+    min: 0,
+    max: 1,
+
+    ticks: {
+        callback: value => value ? "ON" : "OFF"
+    },
+
+    grid: {
+        drawOnChartArea: false
+    }
+}
