45afc4d8d43048c9df7b947794ecd616cd5a4d3f
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-29 17:19:39 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,11 @@
-psuStateData.push(data.psu_output ? 1 : 0);
+function resetData() {
+
+    labels.length = 0;
+    dmmData.length = 0;
+    stateData.length = 0;
+    psuStateData.length = 0;
+    eventLabels.length = 0;
+    eventMarkers.length = 0;
+
+    chart.update();
+}
