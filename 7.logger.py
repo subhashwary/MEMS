62701b8f6b413b7c3a73efb079c7b12048256e77
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 18:10:28 +0530

    Update 7.logger.py

--- 7.logger.py
+++ 7.logger.py
@@ -4,28 +4,25 @@ from datetime import datetime
 
 CSV_FILE = "sensor_data.csv"
 
-
 def initialize_csv():
-    if not os.path.exists(CSV_FILE):
-        with open(CSV_FILE, "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([
-                "Timestamp",
-                "Pressure",
-                "DMM_Voltage",
-                "PSU_Voltage",
-                "PSU_Current",
-                "Mode",
-                "ESS_State",
-                "Cycle",
-                "Event"
-            ])
+
+    with open(CSV_FILE, "w", newline="") as f:
+
+        writer = csv.writer(f)
+
+        writer.writerow([
+            "Timestamp",
+            "DMM_Voltage",
+            "PSU_Voltage",
+            "Mode",
+            "ESS_State",
+            "Cycle",
+            "Event"
+        ])
 
 def log_data(
-    pressure,
     dmm_voltage,
     psu_voltage,
-    psu_current,
     mode,
     ess_state,
     cycle,
@@ -37,10 +34,8 @@ def log_data(
 
         writer.writerow([
             datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            pressure,
             dmm_voltage,
             psu_voltage,
-            psu_current,
             mode,
             ess_state,
             cycle,
