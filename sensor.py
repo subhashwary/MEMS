abbaabdc3fdb4f14a1a15fc0243c1577e051e32f
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 11:48:33 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,20 @@
 timestamp = datetime.now().strftime("%H:%M:%S.%f")[:-3]
+
+def background_worker():
+
+    while True:
+
+        system_state["timestamp"] = (
+            datetime.now()
+            .strftime("%H:%M:%S.%f")[:-3]
+        )
+
+        # ESS Logic
+
+        # PSU Read
+
+        # DMM Read
+
+        # CSV Logging
+
+        time.sleep(0.1)
