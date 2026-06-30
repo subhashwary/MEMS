4d88bb08724475759b005e381d3268f82a05e58b
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 12:19:03 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,7 +1,13 @@
-system_state["event_timestamp"] = (
-    datetime.now().strftime("%H:%M:%S.%f")[:-3]
-)
+def ess_loop():
 
-system_state["event_name"] = (
-    f"Cycle {cycle_no} OFF"
-)
+    while True:
+
+        update_ess_state()
+
+        read_psu()
+
+        read_dmm()
+
+        log_csv()
+
+        time.sleep(0.1)
