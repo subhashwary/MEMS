c6379223d77cb4b96098eddf196e2877197a2404
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 14:16:01 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,4 +1,51 @@
-   if (
-            system_state["mode"] == "auto"
-            and system_state["auto_running"]
+PSU_READ_INTERVAL = 1
+
+        last_psu_read = system_state.get(
+            "last_psu_read",
+            0
+        )
+
+        if (
+            psu
+            and not psu_busy
+            and (time.time() - last_psu_read > PSU_READ_INTERVAL)
         ):
+
+            try:
+
+                with psu_lock:
+
+                    if system_state["psu_output"]:
+
+                        v = psu.query("VOUT1?")
+                        i = psu.query("IOUT1?")
+
+                    else:
+
+                        v = "0"
+                        i = "0"
+
+                print("PSU V =", v)
+                print("PSU I =", i)
+
+                if not v or not i:
+
+                    print("WARNING")
+
+                else:
+
+                    system_state["psu_voltage"] = parse_value(v)
+                    system_state["psu_current"] = parse_value(i)
+
+                    system_state["last_psu_read"] = time.time()
+
+            except Exception as e:
+
+                print(e)
+
+                try:
+                    psu.close()
+                except:
+                    pass
+
+                psu = None
