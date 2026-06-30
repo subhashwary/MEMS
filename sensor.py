1178f25448113e2fd5bd6b9b3880e42b5c9f6644
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 12:09:24 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,1 +1,15 @@
-"timestamp": "",
+if(data.event_timestamp !== lastEventTime){
+
+    lastEventTime = data.event_timestamp;
+
+    eventMarkers.push({
+
+        index: sampleNumber - 1,
+
+        title: data.event_name,
+
+        time: data.event_timestamp
+
+    });
+
+}
