0bba01bbf08eebe5aff88635ef83c88a0433728c
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 17:20:19 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,8 +1,74 @@
-system_state["auto_running"] = False
-system_state["mode"] = "manual"
+@app.route('/mode', methods=['POST'])
+def set_mode():
 
-system_state["dmm_running"] = False
+    global psu
 
-system_state["current_cycle"] = cycles
+    data = request.json or {}
+    mode = data.get("mode")
 
-system_state["psu_output"] = False
+    print("MODE REQUEST =", mode)
+
+    if mode == "manual":
+
+        system_state["mode"] = "manual"
+        system_state["auto_running"] = False
+        system_state["dmm_running"] = False
+
+        if psu and system_state["psu_output"]:
+            try:
+                with psu_lock:
+                    psu.write("OUT0")
+                    time.sleep(0.2)
+            except Exception as e:
+                print("PSU OFF ERROR:", e)
+
+        system_state["psu_output"] = False
+        system_state["psu_voltage"] = 0.0
+        system_state["psu_current"] = 0.0
+
+        system_state["ess_state"] = "IDLE"
+        system_state["cycle_event"] = "Waiting"
+        system_state["current_cycle"] = 0
+
+        return jsonify({
+            "status": "manual mode"
+        })
+
+
+    elif mode == "auto":
+
+        if not psu:
+            return jsonify({
+                "error": "Connect PSU first"
+            }), 400
+
+        if not dmm:
+            return jsonify({
+                "error": "Connect DMM first"
+            }), 400
+
+        system_state["mode"] = "auto"
+        system_state["auto_running"] = True
+
+        system_state["cycle_start"] = time.time()
+
+        system_state["current_cycle"] = 0
+
+        system_state["ess_state"] = "INITIAL_DELAY"
+        system_state["cycle_event"] = "Starting ESS"
+
+        system_state["dmm_running"] = False
+
+        system_state["psu_output"] = False
+        system_state["psu_voltage"] = 0.0
+        system_state["psu_current"] = 0.0
+
+        print("AUTO MODE STARTED")
+
+        return jsonify({
+            "status": "auto mode"
+        })
+
+    return jsonify({
+        "error": "Invalid mode"
+    }), 400
