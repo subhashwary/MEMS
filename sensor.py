a5c7edadcee6125446b5ee6fc3e765bf6646d1b8
Subhash Wary <61883247+subhashwary@users.noreply.github.com>
2026-06-30 16:26:43 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,29 +1,17 @@
-if psu and not system_state["psu_output"]:
+if psu and system_state["psu_output"]:
 
     try:
 
-        voltage = system_state["config"]["voltage"]
-        current = system_state["config"]["current"]
-
         with psu_lock:
-
-            psu.write(f"VSET1:{voltage:.3f}")
+            psu.write("OUT0")
             time.sleep(0.2)
 
-            psu.write(f"ISET1:{current:.3f}")
-            time.sleep(0.2)
+        system_state["psu_output"] = False
+        system_state["psu_voltage"] = 0.0
+        system_state["psu_current"] = 0.0
 
-            psu.write("OUT1")
-            time.sleep(0.2)
-
-        system_state["psu_output"] = True
-        system_state["psu_voltage"] = voltage
-        system_state["psu_current"] = current
-
-        print(f"Cycle {cycle_no}: PSU ON")
-        print(f"Voltage = {voltage} V")
-        print(f"Current = {current} A")
+        print(f"Cycle {cycle_no}: PSU OFF")
 
     except Exception as e:
 
-        print("PSU ON ERROR:", e)
+        print("PSU OFF ERROR:", e)
